6a73af7f780bfac41a8b849447f5926453946889
Yogita.a.Malhotra <yogita.a.malhotra@accenture.com>
2020-05-22 15:17:06 +0530

    Initial Commit

---
+++ DemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by Yogita.a.Malhotra on 22/05/20.
+//  Copyright © 2020 accenture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
