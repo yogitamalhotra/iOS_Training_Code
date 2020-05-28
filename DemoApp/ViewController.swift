f5ff1e2f17b2da25696082c2e54ea47218b58ba6
Yogita.a.Malhotra <yogita.a.malhotra@accenture.com>
2020-05-28 13:35:17 +0530

    First commit

--- DemoApp/ViewController.swift
+++ DemoApp/ViewController.swift
@@ -10,25 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var myLable: UILabel!
-    
-    @IBOutlet var myLabel1: UILabel!
     @IBOutlet var myButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        myLable.text = "Changed Text"
-        myLabel1.text = "Changed Text Label 2"
-
+        myLable.text = "Lable Text 1"
     }
-
     @IBAction func myButtonClicked(_ sender: UIButton) {
         print(sender.classForCoder)
-        print(sender.superclass)
-        
+        print(sender.superclass as Any)
         myButton.setTitle("New button", for: UIControl.State.normal)
+        let alertController = UIAlertController(title: "Welcome to My First app", message: "Hello world", preferredStyle: UIAlertController.Style.alert)
         
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
     }
-
 }
 
